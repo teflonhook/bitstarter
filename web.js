93f12de9e02b973e4e5deb570c6de3d9c34e6aaa
Matt Barrie <matt@freelancer.com>
2013-07-10 13:21:19 +0000

    Part 1

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	var fs = require('fs');
+	var infile = "index.html";
+	var contents = Buffer.toString(fs.readFileSync(infile));
+	response.send(contents);
 });
 
 var port = process.env.PORT || 5000;
