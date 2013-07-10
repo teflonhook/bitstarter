b45db54b323a80c95b5777c8ff05b7707699862a
Matt Barrie <matt@freelancer.com>
2013-07-10 13:24:58 +0000

    Fixed Part 1

--- web.js
+++ web.js
@@ -5,9 +5,9 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 	var fs = require('fs');
 	var infile = "index.html";
-	var contents = fs.readFileSync(infile).toString;
-	response.send(contents);
-});
+	var contents = fs.readFileSync(infile);
+	response.send(contents.toString);
+})
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
