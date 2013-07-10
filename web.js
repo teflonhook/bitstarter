d8fd931d93329139b6c7f77642cf0bc96a20ff1e
Matt Barrie <matt@freelancer.com>
2013-07-10 13:23:31 +0000

    Fixed Part 1

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 	var fs = require('fs');
 	var infile = "index.html";
-	var contents = Buffer.toString(fs.readFileSync(infile));
+	var contents = fs.readFileSync(infile).toString;
 	response.send(contents);
 });
 
